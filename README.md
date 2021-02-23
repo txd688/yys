b85c5f4d21dcdf88513c2a0572956bbc7fe6600e
txd <63951240@qq.com>
2021-02-23 21:43:38 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# yys
-阴阳师自动刷魂土脚本
+## 阴阳师自动刷魂土脚本
+使用指南  
+需要手动输入坐标(可以通过ps等软件)。  
+
+使用语言: python
+库:  
+pip3 install pillow
+#pip3 install pyautogui
---
+++ index.py
@@ -0,0 +1,25 @@
+import pyautogui
+import random
+import time
+
+# 这是作为房主有个'挑战'按钮事件
+
+# 产生随机数
+def randNum(x, y):
+    return round(random.uniform(x, y),3);
+
+n = 0;
+
+while True:
+    # 点击挑战按钮,有两个坐标分别为x(randNum(1320,1377))和y(randNum(1320,1377))，可以自由设置。
+    pyautogui.click(x=randNum(1320,1377), y=randNum(720,778), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear);
+    # 随机游戏时间，我的设置在15s-28s之间。可以自由设置
+    time.sleep(randNum(15,28));
+    # 这是游戏结束后的动画，连点3下(如果不需要可以去掉)
+    pyautogui.click(x=randNum(100,1377), y=randNum(80,300), clicks=3, interval=randNum(1,3), button='left', duration=randNum(1,3), tween=pyautogui.linear);
+    # 这是点击屏幕继续的页面，返回到主页
+    pyautogui.click(x=randNum(100,1377), y=randNum(80,600), clicks=1, interval=randNum(1,3), button='left', duration=randNum(1,3), tween=pyautogui.linear);
+    n += 1;
+    print(n);
+    time.sleep(randNum(1,3));
+   
